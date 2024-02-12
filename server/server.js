e0cbdd2d8fabb8ac73b0ebba51dc4bcbe49184bb
Collin <cdchamberlin.rva@gmail.com>
2024-02-12 18:42:04 -0500

    Added options for CORS. Made sure the front could call the back without error.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ let dbConnect = require("./dbConnect");
 let userRoutes = require('./routes/userRoutes');
 
 const PORT = process.env.PORT;
-app.use(cors());
+const corsOptions = {
+  origin: `http://localhost:${process.env.CLIENT_PORT}`,
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use('/api/users', userRoutes);
