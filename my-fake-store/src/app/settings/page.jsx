662489d1585fd433177403a243163986043516be
Collin <cdchamberlin.rva@gmail.com>
2024-02-13 21:05:23 -0500

    Updated Navbar to include new links to the settings page. Updated Navbar to show either login or logout and settings. Created the settings page, along with needed components AccountForm and AccountInfo.

--- my-fake-store/src/app/settings/page.jsx
+++ my-fake-store/src/app/settings/page.jsx
@@ -1,13 +1,151 @@
-'use client'
+"use client";
 
 import { useUser } from "@/context/UserContext";
+import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
-export default function Settings(){
-    const user = useUser()
-    console.log(`Setting ${currentUser}`)
+function AccountInfo() {
+  const user = useUser();
+  return (
+    <Paper style={{ padding: 20 }}>
+      <Typography variant="h5">Current Account Information</Typography>
+      <Typography>
+        Name: {user.firstName} {user.lastName}
+      </Typography>
+      <Typography>Email: {user.email}</Typography>
+      <Typography>
+        Address: {user.street}, {user.aptnumber}, {user.city}, {user.state},{" "}
+        {user.zipcode}
+      </Typography>
+    </Paper>
+  );
+}
 
-    return(
-        <h2>{`Welcome`}</h2>
+function AccountForm() {
+  const { user, updateUser } = useUser();
+  const [formData, setFormData] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    street: user.street || "",
+    aptnumber: user.aptnumber || "",
+    city: user.city || "",
+    state: user.state || "",
+    zipcode: user.zipcode || "",
+  });
 
-    )
+  const [oldEmail, setOldEmail] = useState(user.email)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleUpdateUser = () => {
+    updateUser({ ...user, ...formData });
+  };
+
+  return (
+    <Paper style={{ padding: 20 }}>
+      <Typography variant="h5">Change Account Information</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="First Name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Last Name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Street"
+            name="street"
+            value={formData.street}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Apartment Number"
+            name="aptnumber"
+            value={formData.aptnumber}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="City"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="State"
+            name="state"
+            value={formData.state}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Zipcode"
+            name="zipcode"
+            value={formData.zipcode}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            onClick={handleUpdateUser}
+            variant="contained"
+            color="primary"
+          >
+            Save Changes
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+export default function Settings() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <AccountInfo />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <AccountForm />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
