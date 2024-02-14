5cb0b8ae6dfd6562ba527045a18ef502c9991bc3
Collin <cdchamberlin.rva@gmail.com>
2024-02-13 22:03:31 -0500

    Finalizing all files.

--- my-fake-store/context/UserContext.jsx
+++ my-fake-store/context/UserContext.jsx
@@ -1,29 +1,41 @@
 "use client";
-import { createContext, useContext, useState } from "react";
-
 import api from "@/api";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useState } from "react";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const router = useRouter();
 
   const login = (userData) => {
     // Logic to handle login
     // Circle Back after MVP
     setCurrentUser(userData);
   };
+  const logout = () => {
+    setCurrentUser(null);
+    router.push("/");
+  };
 
   const createAccount = async (userData) => {
     try {
       // Send a POST request to the backend API endpoint to create a new user
       const response = await api.post("/users/create", userData);
+      setCurrentUser(userData);
     } catch (error) {
       console.error("Error creating user:", error);
       throw error; // Rethrow the error to handle it in the component
     }
-
-    setCurrentUser(userData);
+  };
+  const updateUser = async (userData) => {
+    try {
+      const response = await api.put(`/users/${currentUser.email}`, userData);
+    } catch (error) {
+      console.error("Error updating user: ", error);
+      throw error;
+    }
   };
 
   return (
@@ -33,6 +45,8 @@ export const UserProvider = ({ children }) => {
         setUser: setCurrentUser,
         login,
         createAccount,
+        logout,
+        updateUser,
       }}
     >
       {children}
