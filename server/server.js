90a23d5d7c91149044274a796c1887339e4e1b44
Collin <cdchamberlin.rva@gmail.com>
2024-03-05 23:09:53 -0500

    Added productRoutes to server file.

--- server/server.js
+++ server/server.js
@@ -3,17 +3,19 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 let dbConnect = require("./dbConnect");
-let userRoutes = require('./routes/userRoutes');
+let userRoutes = require("./routes/userRoutes");
+let productRoutes = require("./routes/productRoutes");
 
 const PORT = process.env.PORT;
 const corsOptions = {
   origin: `http://localhost:${process.env.CLIENT_PORT}`,
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 app.get("/api/home", (req, res) => {
   res.json({ message: "Hello World" });
